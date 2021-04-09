da5f772d45ce86507219da6e09d3f70e6bd19d03
abbeyroyse13 <heatherruan126@gmail.com>
2021-04-09 16:07:51 -0500

    starting on edit

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,30 +1,54 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React, { useState } from "react"
+import { Route, Redirect } from "react-router-dom"
 import { LocationList } from "./locations/LocationList"
 import { CustomerList } from "./customers/CustomerList"
 import { EmployeeList } from "./employees/EmployeeList"
 import { Home } from "./Home"
 import { AnimalList } from "./animal/AnimalList"
 import { AnimalDetail } from "./animal/AnimalDetail"
+import { LocationDetail } from "./locations/LocationDetail"
+import { CustomerDetail } from "./customers/CustomerDetail"
+import { EmployeeDetail } from "./employees/EmployeeDetail"
 import { AnimalForm } from "./animal/AnimalForm"
 import { LocationForm } from "./locations/LocationForm"
 import { CustomerForm } from "./customers/CustomerForm"
 import { EmployeeForm } from "./employees/EmployeeForm"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { AnimalEditForm } from "./animal/AnimalEditForm"
 
 export const ApplicationViews = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("kennel_customers") !== null)
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("kennel_customers", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("kennel_customers") !== null)
+    }
+
     return (
         <>
             <Route exact path="/">
                 <Home />
             </Route>
 
-            {/* Make sure you add the `exact` attribute here */}
             <Route exact path="/animals">
-                <AnimalList />
+                {isAuthenticated ? <AnimalList /> : <Redirect to="/login" />}
+            </Route>
+
+            <Route path="/login">
+                <Login setAuthUser={setAuthUser} />
+            </Route>
+
+            <Route path="/register">
+                <Register setAuthUser={setAuthUser} />
             </Route>
 
             <Route path="/animals/create">
                 <AnimalForm />
+            </Route>
+
+            <Route path="/animals/:animalId(\d+)">
+                <AnimalDetail />
             </Route>
 
             <Route exact path="/locations">
@@ -35,8 +59,16 @@ export const ApplicationViews = () => {
                 <LocationForm />
             </Route>
 
+            <Route path="/locations/:locationId(\d+)">
+                <LocationDetail />
+            </Route>
+
             <Route exact path="/customers">
                 <CustomerList />
+            </Route>
+
+            <Route path="/customers/:customerId(\d+)">
+                <CustomerDetail />
             </Route>
 
             <Route path="/customers/create">
@@ -47,9 +79,14 @@ export const ApplicationViews = () => {
                 <EmployeeList />
             </Route>
 
+            <Route path="/employees/:employeeId(\d+)">
+                <EmployeeDetail />
+            </Route>
+
             <Route path="/employees/create">
                 <EmployeeForm />
             </Route>
         </>
     )
 }
+// go back & add in details for customer & employee 
